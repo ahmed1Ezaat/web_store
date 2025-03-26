f8d8bafb4415d9db9e588e46d530ba749700b34f
ahmed1Ezaat <ezzat_ahmed20@yahoo.com>
2025-03-27 00:58:32 +0200

    welcom

--- lib/features/screens/customer/customer_detall/customer.dart
+++ lib/features/screens/customer/customer_detall/customer.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_local_variable
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:yt_ecommerce_admin_panel/features/screens/customer/customer_detall/customer_detail_desktop.dart';
--- lib/features/screens/login/responsive_screens/widgets/validation.dart
+++ lib/features/screens/login/responsive_screens/widgets/validation.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: body_might_complete_normally_nullable
+
 class TValidator {
   static String? validateEmptyText(String? filedName, String? value) {
     if (value == null || value.isEmpty) {
